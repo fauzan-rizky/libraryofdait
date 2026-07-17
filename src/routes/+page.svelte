5b962f5f19b2776dd67e0c8f57fd96a916576c6f
fauzan-rizky <fauzan.rizky.naufal@gmail.com>
2026-07-17 21:41:45 +0700

    gila gw bangga bgt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import "../styles/global.css"
+    import "../styles/text.css"
+
+    let topic = "tech"
+</script>
+<div class="fullScreenContainer" id="home">
+    <h1 id="title">
+        Library of DAIT
+    </h1>
+    <h2 id="subtitle">
+        bahan belajar (topic) berbahasa Indonesia
+    </h2>
+</div>
+<style>
+    h1, h2 {
+        background-color: rgba(0, 0, 0, 0);
+        font-family: "Jersey 25", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+
+  text-shadow: 0px 0px 20px rgb(0, 0, 0);
+    }
+    #title {
+        color: #FE0AB9;
+    }
+
+    #subtitle {
+        color: #29f064;
+    }
+
+    #home {
+        background: url("../images/cpbg.jpg");
+    }
+</style>
